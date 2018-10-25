24da57bacf3c12e7d36719b2e32d81164cbe4188
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-25 18:55:19 +0200

    Add UsersCards component

---
+++ resources/js/components/users/UsersCards.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="row">
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <div class="card-header">First user</div>
+                <div class="card-body">
+                    <h1>this is body</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <div class="card-header">First user</div>
+                <div class="card-body">
+                    <h1>this is body</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <div class="card-header">First user</div>
+                <div class="card-body">
+                    <h1>this is body</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <div class="card-header">First user</div>
+                <div class="card-body">
+                    <h1>this is body</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "UsersCards"
+};
+</script>
+
+<style scoped>
+</style>
--- resources/js/components/users/UsersCardsContainer.vue
+++ resources/js/components/users/UsersCardsContainer.vue
@@ -1,69 +1,24 @@
 <template>
-            <div class="card">
-                <div class="card-header bg-primary text-white">
-                    <span class="pull-left">Users</span>
-                </div>
-
-                <div class="card-body">
-                   <div class="row">
-                        <div class="col-md-4 mb-3">
-                            <div class="card">
-                                <div class="card-header">
-                                    First user
-                                </div> 
-
-                                <div class="card-body">
-                                    <h1>this is body</h1>
-                                </div>
-                            </div>
-                        </div>
-
-                          <div class="col-md-4 mb-3">
-                            <div class="card">
-                                <div class="card-header">
-                                    First user
-                                </div> 
-
-                                <div class="card-body">
-                                    <h1>this is body</h1>
-                                </div>
-                            </div>
-                        </div>
-
-                          <div class="col-md-4 mb-3">
-                            <div class="card">
-                                <div class="card-header">
-                                    First user
-                                </div> 
-
-                                <div class="card-body">
-                                    <h1>this is body</h1>
-                                </div>
-                            </div>
-                        </div>
-
-                          <div class="col-md-4 mb-3">
-                            <div class="card">
-                                <div class="card-header">
-                                    First user
-                                </div> 
-
-                                <div class="card-body">
-                                    <h1>this is body</h1>
-                                </div>
-                            </div>
-                        </div>
-                   </div>
-                </div>
-            </div>
+    <div class="card">
+        <div class="card-header bg-primary text-white">
+            <span class="pull-left">Users</span>
+        </div>
+        <div class="card-body">
+            <UsersCards/>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
-        name : 'UsersCardsContainer',
-    }
+import UsersCards from './UsersCards'
+
+export default {
+  name: "UsersCardsContainer",
+  components : {
+      UsersCards,
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
