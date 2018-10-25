eeab2f54d55e7371292f866d2e9ae6d6731654e9
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-25 22:26:21 +0200

    Remove duplicate rule from UpdateUserRequest

--- app/Http/Requests/User/UpdateUserRequest.php
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -9,7 +9,6 @@ class UpdateUserRequest extends BaseRequest
     public function requestRules() : array
     {
         return [
-            'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users,email,' . $this->route('user')->id,
         ];
     }
