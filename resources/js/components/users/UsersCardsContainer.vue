443e4637f663e66ee35e02c79f0f098e3b455281
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-25 20:19:42 +0200

    Add search input to filter users by name

--- resources/js/components/users/UsersCardsContainer.vue
+++ resources/js/components/users/UsersCardsContainer.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="card">
     <div class="card-header bg-primary">
-      <span class="float-left text-white">Users</span>
-      <button class="btn btn-sm btn-default float-right">Create</button>
+      <div class="row">
+        <div class="col-md-4">
+          <span class="float-left text-white">Users</span>
+        </div>
+        <div class="col-md-4">
+          <input
+            type="text"
+            class="form-control form-control-sm"
+            placeholder="Search For User By Name"
+          >
+        </div>
+        <div class="col-md-4">
+          <button class="btn btn-sm btn-default float-right">Create</button>
+        </div>
+      </div>
     </div>
     <div class="card-body">
       <UsersCards :users="users"/>
