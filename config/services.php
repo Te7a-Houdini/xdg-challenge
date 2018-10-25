deed9820f3d3530dc2887acdf4ebac8ddd6800e3
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-25 18:02:32 +0200

    Add facebook configurations in services.php

--- config/services.php
+++ config/services.php
@@ -12,7 +12,7 @@ return [
     | default location for this type of information, allowing packages
     | to have a conventional place to find your various credentials.
     |
-    */
+ */
 
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
@@ -39,6 +39,12 @@ return [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => url('social-auth/google/callback'),
+    ],
+
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => url('social-auth/facebook/callback'),
     ],
 
 ];
