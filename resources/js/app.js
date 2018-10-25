daeef8f0fa783396578d65da5b505207077f9cb7
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-25 18:50:52 +0200

    Add UsersCardsContainer

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,7 +15,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('users-cards-container', require('./components/users/UsersCardsContainer.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,69 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-header bg-primary text-white">
-                    <span class="pull-left">Users</span>
-                </div>
-
-                <div class="card-body">
-                   <div class="row">
-                        <div class="col-md-4 mb-3">
-                            <div class="card">
-                                <div class="card-header">
-                                    First user
-                                </div> 
-
-                                <div class="card-body">
-                                    <h1>this is body</h1>
-                                </div>
-                            </div>
-                        </div>
-
-                          <div class="col-md-4 mb-3">
-                            <div class="card">
-                                <div class="card-header">
-                                    First user
-                                </div> 
-
-                                <div class="card-body">
-                                    <h1>this is body</h1>
-                                </div>
-                            </div>
-                        </div>
-
-                          <div class="col-md-4 mb-3">
-                            <div class="card">
-                                <div class="card-header">
-                                    First user
-                                </div> 
-
-                                <div class="card-body">
-                                    <h1>this is body</h1>
-                                </div>
-                            </div>
-                        </div>
-
-                          <div class="col-md-4 mb-3">
-                            <div class="card">
-                                <div class="card-header">
-                                    First user
-                                </div> 
-
-                                <div class="card-body">
-                                    <h1>this is body</h1>
-                                </div>
-                            </div>
-                        </div>
-
-                        
-
-
-
-
-                   </div>
-                </div>
-            </div>
+            <users-cards-container/>
         </div>
     </div>
 </div>
