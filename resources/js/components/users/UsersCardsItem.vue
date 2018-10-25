a58ab76e9146c6a5d928780388271bb257469cc8
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-25 19:14:03 +0200

    Modify UsersCardsItem style

--- resources/js/components/users/UsersCardsItem.vue
+++ resources/js/components/users/UsersCardsItem.vue
@@ -1,9 +1,27 @@
 <template>
     <div class="col-md-4 mb-3">
         <div class="card">
-            <div class="card-header">First user</div>
+            <div class="card-header">
+                <h4>First User</h4>
+            </div>
             <div class="card-body">
-                <h1>this is body</h1>
+                <ul>
+                    <li>
+                        <h5>Email : test@example.com</h5>
+                    </li>
+                    <li>
+                        <h5>Name : Ahmed</h5>
+                    </li>
+                    <li>
+                        <h5>Created At : 2018-09-10</h5>
+                    </li>
+                </ul>
+                <a href="#" class="card-link">
+                    <i class="fa fa-lg fa-edit text-default"></i>
+                </a>
+                <a href="#" class="card-link float-right">
+                    <i class="fa fa-lg fa-trash text-danger"></i>
+                </a>
             </div>
         </div>
     </div>
