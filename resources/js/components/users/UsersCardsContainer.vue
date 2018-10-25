25ab25116207e34830ce1f96ae7c3547ed81e46a
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-25 19:30:09 +0200

    Add create button to users card header

--- resources/js/components/users/UsersCardsContainer.vue
+++ resources/js/components/users/UsersCardsContainer.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="card">
-        <div class="card-header bg-primary text-white">
-            <span class="pull-left">Users</span>
+        <div class="card-header bg-primary">
+            <span class="float-left text-white">Users</span>
+            <button class="btn btn-sm btn-default float-right">Create</button>
         </div>
         <div class="card-body">
             <UsersCards :users="[]"/>
