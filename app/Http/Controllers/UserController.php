faa5c0e56e66dc703ff86e9667cb4e2fca6e84d6
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-25 19:52:00 +0200

    Add returning all users from UserController@index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use App\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        
+        return UserResource::collection(User::all());
     }
 }
