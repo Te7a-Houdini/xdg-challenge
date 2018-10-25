2729421fc76d05ff3ed50406cee44ba5b2900598
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-25 21:49:31 +0200

    Add resetiing form and errors for UserFormModal when clicking edit or create

--- resources/js/components/users/UserFormModal.vue
+++ resources/js/components/users/UserFormModal.vue
@@ -23,56 +23,70 @@
                             <input
                                 type="text"
                                 class="form-control"
+                                :class="{'is-invalid' : errors.name}"
                                 id="name"
                                 aria-describedby="emailHelp"
                                 placeholder="Enter Name"
                                 v-model="form.name"
                             >
+                            <FormError v-for="error in errors.name" :key="error" :error="error"/>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputEmail1">E-Mail Address</label>
                             <input
                                 type="email"
                                 class="form-control"
+                                :class="{'is-invalid' : errors.email}"
                                 id="exampleInputEmail1"
                                 aria-describedby="emailHelp"
                                 placeholder="Enter Email"
                                 v-model="form.email"
                             >
+                            <FormError v-for="error in errors.email" :key="error" :error="error"/>
                         </div>
                         <template v-if="inCreateMode">
                             <div class="form-group">
                                 <label for="exampleInputPassword1">Password</label>
                                 <input
                                     type="password"
-                                    class="form-control is-invalid"
+                                    class="form-control"
+                                    :class="{'is-invalid' : errors.password}"
                                     id="exampleInputPassword1"
                                     placeholder="Password"
                                     v-model="form.password"
                                 >
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>errors are here</strong>
-                                </span>
+                                <FormError
+                                    v-for="error in errors.password"
+                                    :key="error"
+                                    :error="error"
+                                />
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputPassword2">Password Confirmation</label>
                                 <input
                                     type="password"
-                                    class="form-control is-invalid"
+                                    class="form-control"
+                                    :class="{'is-invalid' : errors.password_confirmation}"
                                     id="exampleInputPassword2"
                                     placeholder="Password"
                                     v-model="form.password_confirmation"
                                 >
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>errors are here</strong>
-                                </span>
+                                <FormError
+                                    v-for="error in errors.password_confirmation"
+                                    :error="error"
+                                    :key="error"
+                                />
                             </div>
                         </template>
                     </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">{{submitButtonTitle}}</button>
+                    <button
+                        @click.prevent="submit"
+                        type="button"
+                        class="btn btn-primary"
+                    >{{submitButtonTitle}}</button>
                 </div>
             </div>
         </div>
@@ -80,12 +94,15 @@
 </template>
 
 <script>
+import FormError from "../FormError";
+
 export default {
   name: "UserFormModal",
 
   data() {
     return {
       inCreateMode: true,
+      errors: {},
 
       form: {
         id: null,
@@ -111,12 +128,16 @@ export default {
     this.$eventBus.$on("UsersCardsContainer__createUserClicked", () => {
       this.inCreateMode = true;
       this.resetForm();
+      this.resetErrors();
 
       $(this.$el).modal("show");
     });
 
     this.$eventBus.$on("UsersCardsItem__editUserClicked", ({ user }) => {
       this.inCreateMode = false;
+      this.resetForm();
+      this.resetErrors();
+
       this.form.name = user.name;
       this.form.email = user.email;
       this.form.id = user.id;
@@ -134,7 +155,37 @@ export default {
         password: "",
         password_confirmation: ""
       };
+    },
+
+    async submit() {
+      try {
+        let { data } = await axios.post("users", {
+          name: this.form.name,
+          email: this.form.email,
+          password: this.form.password,
+          password_confirmation: this.form.password_confirmation
+        });
+      } catch (error) {
+        if (error.response.status === 422) {
+          this.showFormValidationErrors(error.response.data.errors);
+        }
+      }
+    },
+
+    showFormValidationErrors(errors) {
+      this.resetErrors();
+      Object.keys(errors).forEach(errorKey =>
+        this.$set(this.errors, errorKey, errors[errorKey])
+      );
+    },
+
+    resetErrors() {
+      this.errors = {};
     }
+  },
+
+  components: {
+    FormError
   }
 };
 </script>
