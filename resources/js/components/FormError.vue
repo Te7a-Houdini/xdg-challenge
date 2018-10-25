d643a17cadc5cf0aa8648ef5132bd3eea2520a17
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-25 21:37:19 +0200

    Add FormError component

---
+++ resources/js/components/FormError.vue
@@ -0,0 +1,17 @@
+<template>
+    <span class="invalid-feedback" role="alert">
+        <strong>{{error}}</strong>
+    </span>
+</template>
+
+<script>
+export default {
+  name: "FormError",
+  props: {
+    error: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
