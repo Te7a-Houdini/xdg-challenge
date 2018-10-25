36a274737a32fd5ea0f1b716f39098a642edffb5
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-25 22:32:11 +0200

    Add showing delete user confirmation and emitting delete user confirmed event

--- resources/js/components/users/UsersCardsItem.vue
+++ resources/js/components/users/UsersCardsItem.vue
@@ -2,7 +2,7 @@
     <div class="col-md-4 mb-3">
         <div class="card">
             <div class="card-header">
-                <h5> {{user.name}}</h5>
+                <h5>{{user.name}}</h5>
             </div>
             <div class="card-body">
                 <ul>
@@ -16,10 +16,14 @@
                         <h6>Created : {{user.created_at}}</h6>
                     </li>
                 </ul>
-                <a href="#" class="card-link" @click.prevent="$eventBus.$emit('UsersCardsItem__editUserClicked',{user})">
+                <a
+                    href="#"
+                    class="card-link"
+                    @click.prevent="$eventBus.$emit('UsersCardsItem__editUserClicked',{user})"
+                >
                     <i class="fa fa-lg fa-edit text-default"></i>
                 </a>
-                <a href="#" class="card-link float-right">
+                <a @click.prevent="deleteUser" href="#" class="card-link float-right">
                     <i class="fa fa-lg fa-trash text-danger"></i>
                 </a>
             </div>
@@ -35,6 +39,16 @@ export default {
     user: {
       type: Object,
       required: true
+    }
+  },
+
+  methods: {
+    deleteUser() {
+      if (confirm(`Are You Sure Want To Delete ${this.user.name} ?`)) {
+        this.$eventBus.$emit("UsersCardsItem__deleteUserConfirmed", {
+          user: this.user
+        });
+      }
     }
   }
 };
