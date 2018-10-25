45880b36d6d1d1612b17bedacf1d1e50ec829a6d
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-25 18:37:12 +0200

    Add basic style for users cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,69 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header bg-primary text-white">
+                    <span class="pull-left">Users</span>
+                </div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                   <div class="row">
+                        <div class="col-md-4 mb-3">
+                            <div class="card">
+                                <div class="card-header">
+                                    First user
+                                </div> 
 
-                    You are logged in!
+                                <div class="card-body">
+                                    <h1>this is body</h1>
+                                </div>
+                            </div>
+                        </div>
+
+                          <div class="col-md-4 mb-3">
+                            <div class="card">
+                                <div class="card-header">
+                                    First user
+                                </div> 
+
+                                <div class="card-body">
+                                    <h1>this is body</h1>
+                                </div>
+                            </div>
+                        </div>
+
+                          <div class="col-md-4 mb-3">
+                            <div class="card">
+                                <div class="card-header">
+                                    First user
+                                </div> 
+
+                                <div class="card-body">
+                                    <h1>this is body</h1>
+                                </div>
+                            </div>
+                        </div>
+
+                          <div class="col-md-4 mb-3">
+                            <div class="card">
+                                <div class="card-header">
+                                    First user
+                                </div> 
+
+                                <div class="card-body">
+                                    <h1>this is body</h1>
+                                </div>
+                            </div>
+                        </div>
+
+                        
+
+
+
+
+                   </div>
                 </div>
             </div>
         </div>
